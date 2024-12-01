6b6cf8488f4b5b99cbd1172f94d2f3dac6868bbe
Conchobhar <Conchobhar@users.noreply.github.com>
2024-12-01 17:00:01 +0000

    day1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var l1: MutableList<Int> = mutableListOf()
+        var l2: MutableList<Int> = mutableListOf()
+        var diff: MutableList<Int> = mutableListOf()
+        input.forEach { item ->
+            val (i1, i2) = item.split("   ").map{ it.toInt() }
+            l1.add(i1)
+            l2.add(i2)
+        }
+        l1.sort(); l2.sort()
+        l1.zip(l2).forEach { (i1, i2) ->
+            diff.add(abs(i2 - i1))
+        }
+        return diff.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var l1: MutableList<Int> = mutableListOf()
+        var m2: MutableMap<Int, Int> = mutableMapOf()
+        input.forEach { item ->
+            val (i1, i2) = item.split("   ").map{ it.toInt() }
+            l1.add(i1)
+            m2[i2] = m2[i2]?.plus(1) ?: 1
+        }
+        var sum = 0
+        l1.forEach { i1 ->
+            // rolling sum of (k*v if k in m2 else 0)
+            sum  += m2[i1]?.times(i1) ?: 0
+        }
+        return sum
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
